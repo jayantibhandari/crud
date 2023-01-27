40c0db4c55e248b16fdff6c010b5f59a4c32c5a9
Justbishwas <b15hw45@gmail.com>
2023-01-27 23:19:43 +0545

    added show button

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
-
-<!-- form starts here -->
-
 <?php
 
 include('partials/header.php');
 
 // echo '<h5> CRUD OPERATION <h5>';
 
+//create form startes here
 include('components/dataForm.php');
 
 require('database/create.php');
+//create form ends here
 
 ?>
-
-<!---form ends here -->
-
 
 <?php 
 
